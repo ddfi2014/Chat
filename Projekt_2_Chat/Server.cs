f59b939d50544be41d7a82647c18e47d5a44d2a9
112t <tetete@t.de>
2017-03-13 08:45:08 +0100

    a

--- Projekt_2_Chat/Server.cs
+++ Projekt_2_Chat/Server.cs
@@ -30,5 +30,44 @@ namespace Projekt_2_Chat
             }
             return this.srv;
         }
+
+        public void checkDos(String ip, long ms)
+        {
+            //Vergleicht Zeiten der Anmeldeversuche einer gleichen IP-Adresse, bei zu vielen Versuchen in einem bestimmten Zeitintervall wird die IP-Adresse gebannt.
+            DateTime start;
+            TimeSpan time;
+
+            start = DateTime.Now;
+
+            // -- bei TimeSpan weiß ich noch nicht ob das wirklich dafür taugt 
+
+            time = DateTime.Now - start;
+        }
+
+        public void checkSpam()
+        {
+            //Wenn ein User viele Nachrichten in einer kurzen Zeit abschickt wird seine Nachrichtenfunktion für 30 Sekunden blockiert und er bekommt eine Mitteilung darüber.
+            DateTime start;
+            TimeSpan time;
+
+            start = DateTime.Now;
+
+            // --
+
+            time = DateTime.Now - start;
+        }
+        public Boolean checkForBan()
+        {
+            //bei jeder Anmeldung wird geprüft ob der User gebannt ist (banList)
+            return false;
+        }
+
+        public string getTimestamp()
+        {
+            //zeitstempel für nachrichten
+            String timeStamp = DateTime.Now.ToString("HH:mm:ss tt");
+            return timeStamp;
+        }
     }
+}
 }
